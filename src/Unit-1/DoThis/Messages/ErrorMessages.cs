9c2a4fc191ac8ea553c0f9775aaa1bad0cc3fa0e
Mark Brown <markbrown83@outlook.com>
2018-04-15 14:50:54 +0100

    Unit 1 - Lesson 2

---
+++ src/Unit-1/DoThis/Messages/ErrorMessages.cs
@@ -0,0 +1,33 @@
+﻿namespace WinTail.Messages
+{
+    /// <summary>
+    /// Base class for signalling that user input was invalid.
+    /// </summary>
+    public class InputError
+    {
+        public string Reason { get; }
+
+        public InputError(string reason)
+        {
+            Reason = reason;
+        }
+    }
+
+    /// <summary>
+    /// User provided blank input
+    /// </summary>
+    public class NullInputError : InputError
+    {
+        public NullInputError(string reason) : base(reason)
+        {
+        }
+    }
+
+    public class ValidationError : InputError
+    {
+        public ValidationError(string reason) : base(reason)
+        {
+
+        }
+    }
+}
---
+++ src/Unit-1/DoThis/Messages/SuccessMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace WinTail.Messages
+{
+    public class InputSuccess
+    {
+        public string Reason { get; private set; }
+
+        public InputSuccess(string reason)
+        {
+            Reason = reason;
+        }
+    }
+}
